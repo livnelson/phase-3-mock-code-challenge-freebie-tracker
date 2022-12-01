d1466d36716bb73dcb63c1a946095ec2818b97aa
Liv Nelson <livnelson@me.com>
2022-11-30 16:01:09 -0800

    commit tree 8d42e1b0c25357e62cb0ea9e843062d33e2cec82

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,11 @@
-class Freebie < ActiveRecord::Base
+require "pry"
 
+class Freebie < ActiveRecord::Base
+    belongs_to :devs
+    belongs_to :companies
+
+    def print_details
+        
+        binding.pry
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
+google = Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
+rick = Dev.create(name: "Rick")
 Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
@@ -17,5 +17,17 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+10.times do
+    # create a game with random data
+    Freebie.create(
+      item_name: Faker::Camera.brand_with_model,
+      value: rand(300..600),
+      dev_id: Dev.all.sample.id,
+      company_id: Company.all.sample.id
+    )
+  end
+
+ipad = Freebie.create(item_name: "iPad", value: 700, dev_id: rick.id, company_id: google.id)
 
 puts "Seeding done!"
